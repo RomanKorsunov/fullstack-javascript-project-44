8eed7ddeaf0e8c568d0b5b0ee19cb8e2cad92321
Roman Korsunov <roman.alex.korsunov@yandex.ru>
2024-04-06 14:29:49 +0300

    brain-even | implemented early termination of the game with an incorrect answer

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,33 +1,31 @@
 import readlineSync from 'readline-sync';
 
 const isNumberEvenGame = () => {
-	console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
-    const randomNumber = Math.floor(Math.random() * (101 - 1) + 1);   
-	console.log(`'Question:' ${randomNumber}`);
+  const current = 'Current!';
+  const yesIsWrong = "'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, User!";
+  const noIsWrong = "'no' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, User!";
 
-	const answer = readlineSync.question('Your answer: ');
-
+  for (let i = 0; i <= 2; i += 1) {
+    const randomNumber = Math.floor(Math.random() * (101 - 1) + 1);
     const numberIsEven = randomNumber % 2;
-	
-	const current = 'Current!';
-	const yesIsWrong = "'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, User!";
-	const noIsWrong = "'no' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, User!"
-	
-	if (answer === 'yes' && numberIsEven === 0) {
-		console.log(`${current}`);
 
-	} if (answer === 'yes' && numberIsEven !== 0) {
-		console.log(`${yesIsWrong}`);
+    console.log(`'Question:' ${randomNumber}`);
+    const answer = readlineSync.question('Your answer: ');
 
-	} if (answer === 'no' && numberIsEven !== 0) {
-		console.log(`${current}`);
-
-	} if (answer === 'no' && numberIsEven === 0) {
-		console.log(`${noIsWrong}`);
-	}
-	
-	
+    if (answer === 'yes' && numberIsEven === 0) {
+      console.log(`${current}`);
+    } if (answer === 'yes' && numberIsEven !== 0) {
+      console.log(`${yesIsWrong}`);
+      i += 2;
+    } if (answer === 'no' && numberIsEven !== 0) {
+      console.log(`${current}`);
+    } if (answer === 'no' && numberIsEven === 0) {
+      console.log(`${noIsWrong}`);
+      i += 2;
+    }
+  }
 };
 
 isNumberEvenGame();
