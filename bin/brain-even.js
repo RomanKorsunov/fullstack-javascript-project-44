2f454b1fcce051ab9be7d86a744a27824c58c0a5
Roman Korsunov <roman.alex.korsunov@yandex.ru>
2024-04-07 20:50:30 +0300

    have refactored code in the files brain-games & brain-even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,12 +1,9 @@
 #!/usr/bin/env node
 
 import readlineSync from 'readline-sync';
+import greeting from './brain-games.js';
 
 const isNumberEvenGame = () => {
-  const greeting = () => {
-    console.log('Welcome to the Brain Games!');
-  };
-
   greeting();
 
   const nameRequest = () => {
@@ -15,13 +12,9 @@ const isNumberEvenGame = () => {
     return userName;
   };
 
-  const name = nameRequest();
-
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
-  const current = 'Current!';
-  const yesIsWrong = `'yes' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${name}!`;
-  const noIsWrong = `'no' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${name}!`;
+  const name = nameRequest();
 
   for (let i = 0; i <= 2; i += 1) {
     const randomNumber = Math.floor(Math.random() * (101 - 1) + 1);
@@ -30,21 +23,16 @@ const isNumberEvenGame = () => {
     console.log(`'Question:' ${randomNumber}`);
     const answer = readlineSync.question('Your answer: ');
 
-    if (answer !== 'yes' && answer !== 'no') {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${name}!`);
+    const currentAnswer = 'Current!';
+    const wrongAnswer = `'${answer}' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${name}!`;
+    if (answer !== 'yes' && numberIsEven === 0) {
+      console.log(wrongAnswer);
       break;
-    }
-
-    if (answer === 'yes' && numberIsEven === 0) {
-      console.log(`${current}`);
-    } if (answer === 'yes' && numberIsEven !== 0) {
-      console.log(`${yesIsWrong}`);
+    } else if (answer !== 'no' && numberIsEven !== 0) {
+      console.log(wrongAnswer);
       break;
-    } if (answer === 'no' && numberIsEven !== 0) {
-      console.log(`${current}`);
-    } if (answer === 'no' && numberIsEven === 0) {
-      console.log(`${noIsWrong}`);
-      break;
+    } else {
+      console.log(currentAnswer);
     }
 
     if (i === 2) {
--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,11 +1,7 @@
 #!/usr/bin/env node
 
-import nameRequest from '../src/cli.js';
-
 const greeting = () => {
   console.log('Welcome to the Brain Games!');
 };
 
-greeting();
-
-nameRequest();
+export default greeting;
