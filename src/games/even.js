50abef34775e67a2365e47445c03d413753c07ce
Roman Korsunov <roman.alex.korsunov@yandex.ru>
2024-04-10 15:13:53 +0300

    minor corrections according to the linter rules

--- src/games/even.js
+++ src/games/even.js
@@ -6,7 +6,6 @@ const rules = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 const isEvenGame = () => {
   const getRandomInt = (min, max) => Math.floor(Math.random() * (max + 1 - min) + min);
-
   const isEven = (num) => num % 2 === 0;
 
   const generateRound = () => {
