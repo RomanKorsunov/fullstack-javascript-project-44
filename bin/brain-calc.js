33a76791bf0fa712c8b5ef9e991a297036a31738
Roman Korsunov <roman.alex.korsunov@yandex.ru>
2024-04-10 12:55:38 +0300

    add executable files of games: brain-even.js & brain-calc.js

---
+++ bin/brain-calc.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import { calculatorGame } from "../src/games/calc.js";
+
+calculatorGame();
---
+++ bin/brain-even.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import { isEvenGame } from "../src/games/even.js";
+
+isEvenGame();
