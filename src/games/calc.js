ddbf8d7b0c89bbda44115173360f7de30ee1af17
Roman Korsunov <roman.alex.korsunov@yandex.ru>
2024-04-10 12:44:53 +0300

    imported engine from index.js to even.js & calc.js

---
+++ src/games/calc.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+
+import brainEngine from "../index.js";
+
+const rules = "What is the result of the expression?";
+
+const calculatorGame = () => {
+  const getRandomOperator = () => {
+    const operators = ["+", "-", "*"];
+    return operators[Math.floor(Math.random() * operators.length)];
+  };
+
+  const getRandomInts = () => {
+    const randomNumber1 = Math.floor(Math.random() * (101 - 1) + 1);
+    const randomNumber2 = Math.floor(Math.random() * (101 - 1) + 1);
+    return [randomNumber1, randomNumber2];
+  };
+
+  const generateRound = () => {
+    const randomOperator = getRandomOperator();
+    const [randomNumber1, randomNumber2] = getRandomInts();
+    const question = `${randomNumber1} ${randomOperator} ${randomNumber2}`;
+
+    let correctAnswer = 0;
+
+    switch (randomOperator) {
+      case "+":
+        correctAnswer = randomNumber1 + randomNumber2;
+        break;
+      case "-":
+        correctAnswer = randomNumber1 - randomNumber2;
+        break;
+      case "*":
+        correctAnswer = randomNumber1 * randomNumber2;
+        break;
+      default:
+        correctAnswer = null;
+    }
+    return [correctAnswer.toString(), question];
+  };
+
+  brainEngine(rules, generateRound);
+};
+
+calculatorGame();
