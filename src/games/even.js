d1d57c582a0d067eaed0069252136769ec8c4819
Roman Korsunov <roman.alex.korsunov@yandex.ru>
2024-04-10 12:44:53 +0300

    imported engine from index.js to even.js & calc.js

---
+++ src/games/even.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+import brainEngine from "../index.js";
+
+const rules = 'Answer "yes" if the number is even, otherwise answer "no".';
+
+const isEvenGame = () => {
+  const getRandomInt = (min, max) => {
+    return Math.floor(Math.random() * (max + 1 - min) + min);
+  };
+
+  const isEven = (num) => {
+    return num % 2 === 0;
+  };
+
+  const generateRound = () => {
+    const randomNumber = getRandomInt(0, 100);
+    const correctAnswer = isEven(randomNumber) ? "yes" : "no";
+    const question = randomNumber.toString();
+
+    return [correctAnswer, question];
+  };
+
+  brainEngine(rules, generateRound);
+};
+
+isEvenGame();
