d23b7f51fbc13ee93f8f211d98c86d900fbd3abf
Roman Korsunov <roman.alex.korsunov@yandex.ru>
2024-04-11 09:08:38 +0300

    add prime.js with logic game brain-prime

---
+++ src/games/prime.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+import brainEngine from '../index.js';
+
+const rules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const isPrimeGame = () => {
+  const getRandomInt = () => Math.floor(Math.random() * (101 - 1) + 1);
+
+  const isPrime = (number) => {
+    if (number < 2) {
+      return false;
+    }
+
+    for (let i = 2; i <= number; i += 1) {
+      if (number % i === 0) {
+        return false;
+      }
+    }
+    return true;
+  };
+
+  const generateRound = () => {
+    const randomNumber = getRandomInt();
+    const question = randomNumber.toString();
+    const correctAnswer = isPrime(randomNumber) ? 'yes' : 'no';
+
+    return [correctAnswer, question];
+  };
+
+  brainEngine(rules, generateRound);
+};
+
+isPrimeGame();
