4e0434ce2c24656b89eb24b2f08cb7b5b278a41a
Roman Korsunov <roman.alex.korsunov@yandex.ru>
2024-04-11 14:55:03 +0300

    refactored gcd.js

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -6,36 +6,21 @@ import getRandomInt from '../randomInt.js';
 const rules = 'Find the greatest common divisor of given numbers.';
 
 const gcdGame = () => {
-  const getDivisor = (number) => {
-    const divisors = [];
-    for (let i = 1; i <= number; i += 1) {
-      if (number % i === 0) {
-        divisors.push(i);
+    const getGcdNumbers = (number1, number2) => {
+      while (number2 !== 0) {
+        const temp = number2;
+        number2 = number1 % number2;
+        number1 = temp;
       }
-    }
-    return divisors;
-  };
-
-  const getIntersections = (coll1, coll2) => {
-    const newColl = [];
-    for (const item1 of coll1) {
-      for (const item2 of coll2) {
-        if (item1 === item2) {
-          newColl.push(item1);
-        }
-      }
-    }
-    return newColl;
-  };
-
+      return number1;
+    };
+  
   const generateRound = () => {
-    const divisor1 = getDivisor(getRandomInt());
-    const divisor2 = getDivisor(getRandomInt());
+    const number1 = getRandomInt();
+    const number2 = getRandomInt();
 
-    const intersections = getIntersections(divisor1, divisor2);
-
-    const question = `${getRandomInt()} ${getRandomInt()}`;
-    const correctAnswer = Math.max.apply(null, intersections);
+    const question = `${number1} ${number2}`;
+    const correctAnswer = getGcdNumbers(number1, number2);
 
     return [correctAnswer.toString(), question];
   };
