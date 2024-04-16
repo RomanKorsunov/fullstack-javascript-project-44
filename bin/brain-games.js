976587b50b80db76ca9486c57fa4f325cc8a7eed
Roman Korsunov <roman.alex.korsunov@yandex.ru>
2024-04-16 15:14:02 +0300

    delete the greeting from the brain-games file and add it to the file cli.js to make the executable file look more neat

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
-import askForName from '../src/cli.js';
+import makeWelcome from '../src/cli.js';
 
-console.log('Welcome to the Brain Games!');
 
-askForName();
+makeWelcome();
--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,10 @@
 import readlineSync from 'readline-sync';
 
-const askForName = () => {
+const makeWelcome = () => {
+  console.log('Welcome to the Brain Games!');
+
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
 };
 
-export default askForName;
+export default makeWelcome;
