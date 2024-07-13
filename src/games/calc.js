7da1549007fa15d9bbcf88445c23e145216993fe
Roman Korsunov <roman.alex.korsunov@yandex.ru>
2024-07-13 12:41:16 +0300

    do a refactoring calc.js

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,34 +1,42 @@
-import brainEngine from '../index.js';
-import getRandomInRange from '../randomInt.js';
+import brainEngine from "../index.js";
+import getRandomInRange from "../randomInt.js";
 
-const rules = 'What is the result of the expression?';
+const rules = "What is the result of the expression?";
 
 const getRandomOperator = () => {
-  const operators = ['+', '-', '*'];
+  const operators = ["+", "-", "*"];
   return operators[Math.floor(Math.random() * operators.length)];
 };
 
-const calculatorGame = () => {
-  const generateRound = () => {
-    const randomOperator = getRandomOperator();
-    const randomNumber1 = getRandomInRange();
-    const randomNumber2 = getRandomInRange();
-    const question = `${randomNumber1} ${randomOperator} ${randomNumber2}`;
+const calculate = (number1, number2, operator) => {
+  let result = 0;
+  if (operator === "+") {
+    result = number1 + number2;
+  } else if (operator === "-") {
+    result = number1 - number2;
+  } else {
+    result = number1 * number2;
+  }
 
-    let correctAnswer = 0;
-
-    if (randomOperator === '+') {
-      correctAnswer = randomNumber1 + randomNumber2;
-    } else if (randomOperator === '-') {
-      correctAnswer = randomNumber1 - randomNumber2;
-    } else {
-      correctAnswer = randomNumber1 * randomNumber2;
-    }
-
-    return [correctAnswer.toString(), question];
-  };
-
-  brainEngine(rules, generateRound);
+  return result;
 };
+
+const generateRound = () => {
+  const randomOperator = getRandomOperator();
+  const randomNumber1 = getRandomInRange();
+  const randomNumber2 = getRandomInRange();
+
+  const question = `${randomNumber1} ${randomOperator} ${randomNumber2}`;
+
+  const correctAnswer = calculate(
+    randomNumber1,
+    randomNumber2,
+    randomOperator
+  );
+
+  return [correctAnswer.toString(), question];
+};
+
+const calculatorGame = () => brainEngine(rules, generateRound);
 
 export default calculatorGame;
