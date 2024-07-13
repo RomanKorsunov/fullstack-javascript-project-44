57f9dabd3d3662d891643a2661017c42ae51fa84
Roman Korsunov <roman.alex.korsunov@yandex.ru>
2024-07-13 12:44:48 +0300

    lint code

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,18 +1,18 @@
-import brainEngine from "../index.js";
-import getRandomInRange from "../randomInt.js";
+import brainEngine from '../index.js';
+import getRandomInRange from '../randomInt.js';
 
-const rules = "What is the result of the expression?";
+const rules = 'What is the result of the expression?';
 
 const getRandomOperator = () => {
-  const operators = ["+", "-", "*"];
+  const operators = ['+', '-', '*'];
   return operators[Math.floor(Math.random() * operators.length)];
 };
 
 const calculate = (number1, number2, operator) => {
   let result = 0;
-  if (operator === "+") {
+  if (operator === '+') {
     result = number1 + number2;
-  } else if (operator === "-") {
+  } else if (operator === '-') {
     result = number1 - number2;
   } else {
     result = number1 * number2;
@@ -31,7 +31,7 @@ const generateRound = () => {
   const correctAnswer = calculate(
     randomNumber1,
     randomNumber2,
-    randomOperator
+    randomOperator,
   );
 
   return [correctAnswer.toString(), question];
